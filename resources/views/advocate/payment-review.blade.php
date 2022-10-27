32b91c51501172e80b4e116dff10309a1f089cbd
Jaydeep Khokhar <jaydeeppkhokhar@gmail.com>
2022-10-27 10:18:49 +0530

    invoicing changes added

--- resources/views/advocate/payment-review.blade.php
+++ resources/views/advocate/payment-review.blade.php
@@ -274,178 +274,46 @@ request()->session()->forget('response_error_msg');
                             </div>
 
                             <div class="tagline_wrap">
-                                <p>Your Path to daily hydration + wellness</p>
+                                <p style="margin-top: 0 !important;">Your Path to daily hydration + wellness</p>
                             </div>
                         </div>
-                        <input type="hidden" name="payment_method_hidden" id="payment_method_hidden">
-                        <input id="payment_method_nonce" name="payment_method_nonce" type="hidden" />
-                        <input id="payment_method_nonce_update" name="payment_method_nonce_update" type="hidden" />
                         <div class="form_wrapper edit_form_wrapper">
                             <div class="flex_row">
                                 <div class="flex_col_sm_12">
                                     <div class="form_field purchase_page_main_field_custom_css">
                                         <div class="text-field flex_row purchase_page_custom_css">
                                             <div class="final_page_product_label">YOUR PRODUCT</div>
-                                            <select class="selectpicker custom_select" name="product" id="product5" required data-dropup-auto="false" title="YOUR PRODUCT">
-                                                <option value="" data-hidden="true">YOUR PRODUCT</option>
-                                                <option value="1">ALKALINE + ELECTROLYTE</option>
-                                                <option value="2">PURE + ELECTROLYTE</option>
-                                            </select>
-                                            {{-- <button type="button" class="edit btn_effect edit_product">Edit</button> --}}
                                         </div>
-                                        <span class="text-note product_note_final_page">YOUR PRODUCT</span>
+                                        @foreach ($products as $product)
+                                        <span class="text-note product_note_final_page">{{ config('constants.product_name.'.$product->product_name) }}</span>
+                                        @endforeach
                                     </div>
 
                                     <div class="form_field purchase_page_main_field_custom_css">
                                         <div class="text-field flex_row purchase_page_custom_css">
-                                            <div class="final_page_package_label">YOUR WELLNESS SOLUTION</div>
-                                            <select class="selectpicker custom_select final_page_package_select select_alka" name="package" id="package5" required data-dropup-auto="false" title="YOUR WELLNESS SOLUTION">
-                                                <option data-hidden="true" selected>YOUR WELLNESS SOLUTION</option>
-                                                <option value="1">1 WEEK WELLNESS $78 ( 3 KITS )</option>
-                                                <option value="2">1 MONTH WELLNESS $250 ( 10 KITS )</option>
-                                                <option value="3">2 MONTH WELLNESS $500 ( 20 KITS )</option>
-                                            </select>
-
-                                            <select class="selectpicker custom_select final_page_package_select select_pure d-none" name="package" id="package5" required data-dropup-auto="false" title="YOUR WELLNESS SOLUTION">
-                                                <option data-hidden="true" selected>YOUR WELLNESS SOLUTION</option>
-                                                <option value="1">1 WEEK WELLNESS $66 ( 3 KITS )</option>
-                                                <option value="2">1 MONTH WELLNESS $220 ( 10 KITS )</option>
-                                                <option value="3">2 MONTH WELLNESS $440 ( 20 KITS )</option>
-                                            </select>
-
-
-                                            <button type="button" class="edit btn_effect edit_package">Edit</button>
+                                            <div class="final_page_package_label">KITS</div>
                                         </div>
-                                        <span class="text-note package_note_final_page">EVERY SUNDAY FOR 1 MONTH
-                                        </span>
-                                        {{-- <span class="text-note">(1st DELIVERY 4 KITS, THEN 2 KITS THEREAFTER). </span> --}}
+                                        @foreach ($products as $product)
+                                        <span class="text-note product_note_final_page">{{ $product->kits }} ({{ config('constants.product_name.'.$product->product_name) }})</span>
+                                        @endforeach
                                     </div>
 
                                     <div class="form_field purchase_page_main_field_custom_css">
                                         <div class="text-field flex_row purchase_page_custom_css">
-                                            <div class="final_page_delivery_freq_label">DELIVERY FREQUENCY</div>
-                                            <select class="selectpicker custom_select" title="DELIVERY FREQUENCY" name="delivery_frequency" id="delivery_frequency5" required data-dropup-auto="false">
-                                                <option value="" data-hidden="true" title="{{ config('constants.package.delivery_freq_text') }}">
-                                                    {{ config('constants.package.delivery_freq_text') }}</option>
-                                                <option value="1" selected>UPCOMING SUNDAY</option>
-                                                <option value="2">UPCOMING MONDAY</option>
-                                            </select>
-                                            <button type="button" class="edit btn_effect edit_delivery_freq">Edit
-                                            </button>
+                                            <div class="final_page_delivery_freq_label">DELIVERY DATE</div>
                                         </div>
                                         <span class="text-note delivery_note_final_page">EVERY SUNDAY FOR 1 MONTH
                                         </span>
-                                        {{-- <span class="text-note">(1st DELIVERY 4 KITS, THEN 2 KITS THEREAFTER). </span> --}}
                                     </div>
 
 
                                     <div class="form_field purchase_page_main_field_custom_css">
                                         <div class="text-field purchase_page_custom_css">
                                             <div class="final_page_edit_address_label">DELIVERY ADDRESS</div>
-
-                                            <div class="final_page_address_field">
-                                                <div class="text-field">
-                                                    <input type="text" class="shipping_address_final_page" id="shipping_address_final_page" placeholder="DELIVERY ADDRESS 1" disabled>
-                                                </div>
-
-                                                <div class="text-field">
-                                                    <input type="text" class="shipping_address1_final_page" id="shipping_address1_final_page" placeholder="UNIT # | SUITE # | APT" disabled>
-                                                </div>
-
-                                                <div class="row">
-                                                    <div class="text-field col">
-                                                        <input type="text" class="s_city_final_page" id="s_city_final_page" placeholder="CITY" disabled>
-                                                    </div>
-                                                    <div class="form-group col">
-                                                        <input type="text" class="s_state_final_page" id="s_state_final_page" placeholder="STATE">
-                                                    </div>
-                                                    <div class="form-group col">
-                                                        <input type="text" class="s_zip_final_page" id="s_zip_final_page" placeholder="ZIP">
-                                                    </div>
-                                                </div>
-
-                                            </div>
-                                            <button type="button" class="edit btn_effect edit_address_final_page">EDIT</button>
                                         </div>
-                                        <span class="text-note shipping_add_final_page"></span>
-                                        <span class="text-note citi_state_zip_final_page"></span>
+                                        <span class="text-note shipping_add_final_page">{{ $invoiceDataObj->billing_address }} {{ $invoiceDataObj->billing_address2 }}</span>
+                                        <span class="text-note citi_state_zip_final_page">{{ json_decode($invoiceDataObj->b_city_state_zip)->city.', '.json_decode($invoiceDataObj->b_city_state_zip)->state.', '.json_decode($invoiceDataObj->b_city_state_zip)->zip }}</span>
                                     </div>
-
-                                    <div class="form_field purchase_page_main_field_custom_css">
-                                        <div class="text-field flex_row purchase_page_custom_css">
-                                            <div class="final_page_payment_source_label">PAYMENT SOURCE</div>
-                                            <select class="selectpicker custom_select payment_method payment_method_finl_page_class" name="payment_method" id="payment_method" data-dropup-auto="false" title="SELECT PAYMENT METHOD">
-                                                <option data-hidden="true">SELECT PAYMENT METHOD</option>
-                                                <option value="1">CREDIT CARD</option>
-                                                <option value="2">DEBIT CARD</option>
-                                                <option value="3">VENMO</option>
-                                                {{-- <option value="4">APPLY PAY</option> --}}
-                                            </select>
-
-                                            <div id="final_pay_card_deatil">
-                                                <div class="bootstrap-basic card-div-final" id="bt-dropin-final" style="display: none;">
-                                                    <form class="needs-validation" id="form-hosted-final" novalidate>
-                                                        <div class="row">
-                                                            <div class="col-sm-12 mb-6">
-                                                                <div class="form-control" id="cc-name-final"></div>
-                                                                <div class="invalid-feedback">
-                                                                    Name on card is required
-                                                                </div>
-                                                            </div>
-
-                                                        </div>
-                                                        <br>
-
-                                                        <div class="row">
-                                                            <div class="col-sm-12 mb-6">
-
-                                                                <div class="form-control text-field " id="cc-number-final"></div>
-                                                                <span id="card-brand-final"></span>
-
-                                                                <div class="invalid-feedback">
-                                                                    Credit card number is required
-                                                                </div>
-                                                            </div>
-
-                                                        </div>
-                                                        <br>
-
-                                                        <div class="row">
-                                                            <div class="col-6 mb-6">
-
-                                                                <div class="form-control" id="cc-cvv-final"></div>
-                                                                <div class="invalid-feedback">
-                                                                    Security code required
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-6 mb-6">
-
-                                                                <div class="form-control" id="cc-expiration-final"></div>
-                                                                <div class="invalid-feedback">
-                                                                    Expiration date required
-                                                                </div>
-                                                            </div>
-
-                                                        </div>
-
-
-                                                        <div class="text-center">
-                                                            {{-- <button class="btn btn-primary btn-lg" type="submit">Pay with <span id="card-brand">Card</span></button> --}}
-                                                        </div>
-
-                                                </div>
-                                            </div>
-
-                                            <button type="button" class="edit btn_effect edit_pay_final_btn edit_payment_method_final_page">Edit
-                                            </button>
-
-                                            <button type="button" class="edit btn_effect save_payment_method_final_page" style="display: none; top:9%;">Save
-                                            </button>
-                                        </div>
-                                        {{-- <span class="text-note">CARD ENDING IN <b><span class="last_4_digit_card"></span></b></span> --}}
-                                        <span class="text-note payment_method_final_page" style="display: none;">Payment Method</span>
-                                    </div>
-
                                     <div class="form_field purchase_page_main_field_custom_css">
                                         <div class="flex_row">
                                             <div class="col-6 text-left">
@@ -491,32 +359,15 @@ request()->session()->forget('response_error_msg');
 
                             <div class="text-center">
                                 <div class="form_field">
-                                    {{-- <button type="button" class="outline_btn m_r_20 show_step4_form">BACK</button> --}}
-                                    <button type="submit" class="primary_btn btn_effect btn_black purchase_button" style="width: auto;" data-disable-with="Purchasing...">
-                                        PURCHASE
-                                    </button>
-
-                                    <div id="bt-dropin_venmo" style="display: none;">
-                                        <button type="button" id="venmo-button" class="primary_btn btn_effect btn_black" style="width: auto;">
-                                            <img style="border-radius: 20px;" src="{{ asset('images/venmo.png') }}" height="35px" width="35px">
-                                            <b>PURCHASE</b>
-                                        </button>
-                                    </div>
+                                    <button type="submit" class="primary_btn btn_effect btn_black purchase_button" style="width: auto;" data-disable-with="Purchasing...">PAY</button>
                                 </div>
                             </div>
-
-                            <div class="text-center recipt_note">
-                                UPON PURCHASE YOU WILL RECEIVE A RECEIPT VIA TEXT & EMAIL
-                            </div>
-
                         </div>
 
                     </div>
                     @include('footer')
                 </main>
             </div>
-            <!-- Step 5 ends -->
-
         </main>
     </form>
 
@@ -533,23 +384,11 @@ request()->session()->forget('response_error_msg');
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('js/bootstrap-select.js') }}"></script>
 <script src="{{ asset('js/custom.js') }}"></script>
-<script src="https://js.braintreegateway.com/web/dropin/1.33.2/js/dropin.min.js"></script>
-<script src="{{ asset('js/main.js') }}"></script>
-<script src="{{ asset('js/link2.js') }}"></script>
+{{-- <script src="{{ asset('js/main.js') }}"></script> --}}
+{{-- <script src="{{ asset('js/link2.js') }}"></script> --}}
 <script src="{{ asset('js/show2.js') }}"></script>
 <script src="{{ asset('js/aos.js') }}"></script>
 <script src="{{ asset('js/custom.js') }}"></script>
-<script src="https://applepay.cdn-apple.com/jsapi/v1/apple-pay-sdk.js"></script>
-<script src="https://js.braintreegateway.com/web/3.77.0/js/client.min.js"></script>
-<script src="https://js.braintreegateway.com/web/3.77.0/js/venmo.min.js"></script>
-<script src="https://js.braintreegateway.com/web/3.77.0/js/data-collector.min.js"></script>
-<script src="{{ asset('js/venmo.js') }}"></script>
-<script src="https://js.braintreegateway.com/web/3.85.5/js/client.min.js"></script>
-<script src="https://js.braintreegateway.com/web/3.85.3/js/apple-pay.min.js"></script>
-<script src="{{ asset('js/applepay.js') }}"></script>
-<script src='https://js.braintreegateway.com/web/3.85.5/js/hosted-fields.min.js'></script>
-<script src="{{ asset('js/hosted-custom.js') }}"></script>
-<script src="{{ asset('js/hosted-custom-updated.js') }}"></script>
 <script>
     $(document).ready(function() {});
 
